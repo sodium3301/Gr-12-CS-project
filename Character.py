f4e4839d4648e37aca05a8fd559fc1b512ee5cdd
Ian Zeng <sodiumhydroxide526@gmail.com>
2024-12-18 08:40:05 -0500

    changes on stuff class for testing

--- Character.py
+++ Character.py
@@ -2,9 +2,7 @@ import pygame
 from settings import *
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos):
-        #super().__init__(groups)
-        self.surf = pygame.image.load('graphics/rock.jpg').convert_alpha()
-        self.rect = self.surf.get_rect(topleft = pos)
-    
-    
+    def __init__(self, pos, groups):
+        super().__init__(groups)
+        self.image = pygame.image.load('graphics/rock.png').convert_alpha
+        self.rect = self.image.get_rect(topleft = pos)
--- stuff.py
+++ stuff.py
@@ -1,9 +1,16 @@
 import pygame
+from settings import *
+from tiles import Tile
+from Character import Player
 
 class Stuff:
     def __init__(self):
         self.display_surface = pygame.display.get_surface()
         self.visible_sprites = pygame.sprite.Group()
+        self.create_map()
+
+    def create_map(self):
+        Tile((250,250), [self.visible_sprites])
         
     def run(self):
-        pass
+        self.visible_sprites.draw(self.display_surface)
