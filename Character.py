b63ac07b8f7664c5b5b2197d365c8e9fb0c70db0
YihangCai1 <acyll-749@outlook.com>
2025-01-06 12:40:36 -0500

    Update Character.py

--- Character.py
+++ Character.py
@@ -60,6 +60,25 @@ class Player(pygame.sprite.Sprite):
                     if self.direction.y < 0:
                         self.rect.top = sprite.rect.bottom
     
+    def get_direction(self):
+        return self.direction
+
     def update(self):
         self.input()
         self.move(self.speed)
+
+class YSortCameraGroup(pygame.sprite.Group):
+	def __init__(self):
+		super().__init__()
+		self.display_surface = pygame.display.get_surface()
+		self.half_width = self.display_surface.get_size()[0] // 2
+		self.half_height = self.display_surface.get_size()[1] // 2
+		self.offset = pygame.math.Vector2()
+
+	def custom_draw(self,player):
+		self.offset.x = player.rect.centerx - self.half_width
+		self.offset.y = player.rect.centery - self.half_height
+
+		for sprite in sorted(self.sprites(),key = lambda sprite:sprite.rect.centery):
+			offset_pos = sprite.rect.topleft - self.offset
+			self.display_surface.blit(sprite.image,offset_pos)
