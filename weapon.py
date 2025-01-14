ae3f082b66c1c60f5ebcaa1a402b08225f22298d
YihangCai1 <acyll-749@outlook.com>
2025-01-14 12:28:40 -0500

    idk

--- weapon.py
+++ weapon.py
@@ -1,25 +1,26 @@
 import pygame
-class Weapon(pygame.sprite.Sprite):
-    def __init__(self, player, groups):
-        super().__init__(groups)
 
+class Weapon(pygame.sprite.Sprite):
+    def __init__(self,player,groups):
+        super().__init__(groups)
         direction = player.status.split('_')[0]
 
-        # graphics
-        full_path = f'graphics/weapons/{player.weapon}/{direction}.png'
-        # full_path = 'graphics/test/player.png'
-        # full_path = 'graphics/test/player.png'
+
+        #graphics
+        full_path = f'graphics/melee sprites/{direction}.png'
         self.image = pygame.image.load(full_path).convert_alpha()
 
-
-        # placement
-        if direction =='right':
-            self.rect = self.image.get_rect(midleft = player.rect.midright + pygame.math.Vector2(0, 16))
-        elif direction =='left':
-            self.rect = self.image.get_rect(midright = player.rect.midleft + pygame.math.Vector2(0, 16))
-        elif direction =='down':
-            self.rect = self.image.get_rect(midtop = player.rect.midbottom + pygame.math.Vector2(-10, 0))
-        
+        #placement
+        if direction == 'right':
+            self.rect = self.image.get_rect(midleft = player.rect.midright + pygame.math.Vector2(-64, 16))
+        elif direction == 'left':
+            self.rect = self.image.get_rect(midright = player.rect.midleft + pygame.math.Vector2(64, 16))     
+        elif direction == 'down':
+            self.rect = self.image.get_rect(midtop = player.rect.midbottom + pygame.math.Vector2(0, -64))  
+        elif direction == 'up':
+            self.rect = self.image.get_rect(midbottom = player.rect.midtop + pygame.math.Vector2(0, 64))         
         else:
-            self.rect = self.image.get_rect(midbottom = player.rect.midtop + pygame.math.Vector2(10, 0))
-        
+            self.rect = self.image.get_rect(center = player.rect.center)
+
+
+
